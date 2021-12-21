9159b4053b839465b4b55d737574cb7dc72b1eba
Luc Perin Panta <lucapameni@gmail.com>
2021-12-21 11:05:20 +0100

    adding functions

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('register/', RegisterAPI.as_view(), name='register'),
-    path('login', LoginAPI.as_view(), name='login'),
+    path('login/', LoginAPI.as_view(), name='login'),
     path('logout/', knox_views.LogoutView.as_view(), name='logout'),
     path('logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
     path('user/', UserAPI.as_view(), name='user'),
