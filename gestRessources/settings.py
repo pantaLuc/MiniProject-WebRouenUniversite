e5cf6c5ed1f12996921b339c398fc04dd500264e
Luc Perin Panta <lucapameni@gmail.com>
2021-12-22 23:50:43 +0100

    Adding heroku to solved erasing database

--- gestRessources/settings.py
+++ gestRessources/settings.py
@@ -13,10 +13,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-
+#django_heroku.settings(locals())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -164,3 +166,4 @@ CORS_REPLACE_HTTPS_REFERER = True
 #CSRF_TRUSTED_ORIGINS =True
 CSRF_TRUSTED_ORIGINS = ['*']
 CSRF_USE_SESSIONS = False
+django_heroku.settings(locals())
